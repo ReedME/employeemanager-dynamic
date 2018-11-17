ab780650a963bbb1e41da021aac8feead859f326
Brendan Reed <brendan.james.reed@gmail.com>
2018-11-17 20:35:36 +1100

    update

--- src/firebaseService/firebaseServiceConfig.js
+++ src/firebaseService/firebaseServiceConfig.js
@@ -1,10 +1,10 @@
 const prodConfig = {
-    apiKey           : "YOUR_API_KEY",
-    authDomain       : "your-app.firebaseapp.com",
-    databaseURL      : "https://your-app.firebaseio.com",
-    projectId        : "your-app",
-    storageBucket    : "your-app.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID"
+    apiKey: "AIzaSyCsVd5CIs_Mz-JH4bRmJnvck5ko3etGjXc",
+    authDomain: "employee-manager-dev-6e035.firebaseapp.com",
+    databaseURL: "https://employee-manager-dev-6e035.firebaseio.com",
+    projectId: "employee-manager-dev-6e035",
+    storageBucket: "employee-manager-dev-6e035.appspot.com",
+    messagingSenderId: "1060199535232"
 };
 
 const devConfig = {
@@ -12,7 +12,7 @@ const devConfig = {
     authDomain: "employee-manager-dev-6e035.firebaseapp.com",
     databaseURL: "https://employee-manager-dev-6e035.firebaseio.com",
     projectId: "employee-manager-dev-6e035",
-    storageBucket: "",
+    storageBucket: "employee-manager-dev-6e035.appspot.com",
     messagingSenderId: "1060199535232"
 };
 
